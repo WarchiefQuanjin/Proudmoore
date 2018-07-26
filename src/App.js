edcddc100ef059f43ab83670bde0338e025f4089
WarchiefQuanjin <hector_tl4@hotmail.com>
2018-07-26 12:27:00 -0500

    Step 2: Rough version

--- src/App.js
+++ src/App.js
@@ -1,21 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import React, { Component } from 'react'
+class App extends Component {   
+  render() {     
+    return (       
+      <div style={style}>
+         <div>
+           <h1>Export Demo</h1>
+           <button>Export to Excel</button>
+           <table>
+             <tr>
+               <th>Firstname</th>
+               <th>Lastname</th>
+               <th>Age</th>
+             </tr>
+             <tr>
+               <td>Garrosh</td>
+               <td>Hellscream</td>
+               <td>23</td>
+             </tr>
+             <tr>
+               <td>Grommash</td>
+               <td>Hellscream</td>
+               <td>53</td>
+             </tr>
+           </table>
+         </div>
+       </div>
+     )
+   }
+ }
+const style = {
+  display: 'flex',
+  justifyContent: 'center'
 }
+export default App
 
-export default App;
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
